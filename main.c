f87527be94e2b1eed3d9c5cc360b3af6dcce7bbe
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-18 15:18:37 -0600

    ...

--- main.c
+++ main.c
@@ -3,10 +3,12 @@
 #include "electron.h"
 
 int main() {
+  printf("Periodic Table");
   initialize_sublevels();
 
   electron_config helium = create_electron_config(2);
   printf("%d\n", helium.valence_electrons);
+  printf("%d\n", sub_1s.electrons);
 
   return 1;
 }

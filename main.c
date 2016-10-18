24cafc6fa86da1ce79849831b5f8a10234c99bac
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-18 16:41:42 -0600

    ...

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ int main() {
   printf("%d, %d, %d, %d\n", element.atomic_number, element.highest_energy_level, element.valence_electrons, element.charge);
 
   unsigned int atomic_number = 27;
-  unsigned int valence = (((atomic_number - 18) > 2) ? (2 + (((atomic_number - 30) > 0) ? (atomic_number - 30) : 0)) : (atomic_number - 18));
+  int valence = (((atomic_number - 18) > 2) ? (2 + (((atomic_number - 30) > 0) ? (atomic_number - 30) : 0)) : (atomic_number - 18));
   printf("%d\n", valence);
-  
+
   return 1;
 }

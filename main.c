d075df46902d5998ce8d5cb60b521fa501659000
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-18 16:45:47 -0600

    ...

--- main.c
+++ main.c
@@ -7,10 +7,7 @@ int main() {
 
   electron_config element = create_electron_config(27);
   printf("%d, %d, %d, %d\n", element.atomic_number, element.highest_energy_level, element.valence_electrons, element.charge);
-
-  unsigned int atomic_number = 27;
-  int valence = (((atomic_number - 18) > 2) ? (2 + (((atomic_number - 30) > 0) ? (atomic_number - 30) : 0)) : (atomic_number - 18));
-  printf("%d\n", valence);
+  printf("%d\n", (((27 - 18) > 2) ? (((27 - 30) > 0) ? (2 + (27 - 30)) : 2) : 0));
 
   return 1;
 }

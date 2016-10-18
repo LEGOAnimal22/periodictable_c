96752c6997c3ae6529026905c0e64ef39fb9e0df
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-18 15:29:29 -0600

    ...

--- main.c
+++ main.c
@@ -10,5 +10,9 @@ int main() {
   printf("%d\n", helium.valence_electrons);
   printf("%d %d\n", sub_1s.electrons, sub_1s.max_electrons);
 
+  uint32_t x = 4;
+  add_to_sublevel(&sub_1s, &x);
+  printf("%d %d\n", sub_1s.electrons, x);
+
   return 1;
 }

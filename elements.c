702a51dc995a1d323cd339a1201a9ee9ad794cc3
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-20 15:13:00 -0600

    ??

--- elements.c
+++ elements.c
@@ -1,12 +1,15 @@
 // Copyright (c) 2016 LEGOAnimal22
 
+#include <string.h>
 #include "electron.h"
 #include "elements.h"
 
 element create_element(unsigned int atomic_number) {
   electron_config e_cnfg = create_electron_config(atomic_number);
 
-  element new_element = {element_names[atomic_number - 1], element_symbols[atomic_number - 1], atomic_number, e_cnfg};
+  element new_element = {"", "", atomic_number, e_cnfg};
+  strcat(new_element.name, element_names[atomic_number - 1]);
+  strcat(new_element.symbol, element_symbols[atomic_number - 1]);
 
   return new_element;
 }

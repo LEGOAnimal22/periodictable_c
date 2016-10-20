24ff8ce9d7797ed330c4593f371b9916aade879e
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-19 19:44:59 -0600

    ...

---
+++ elements.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2016 LEGOAnimal22
+
+#include "electron.h"
+
+electron_config elements[32] =
+{
+  create_electron_config(1), create_electron_config(2),
+  create_electron_config(3), create_electron_config(4),
+  create_electron_config(5), create_electron_config(6),
+  create_electron_config(7), create_electron_config(8),
+  create_electron_config(9), create_electron_config(10),
+  create_electron_config(11), create_electron_config(12),
+  create_electron_config(13), create_electron_config(14),
+  create_electron_config(15), create_electron_config(16),
+  create_electron_config(17), create_electron_config(18),
+  create_electron_config(19), create_electron_config(20),
+  create_electron_config(21), create_electron_config(22),
+  create_electron_config(23), create_electron_config(24),
+  create_electron_config(25), create_electron_config(26),
+  create_electron_config(27), create_electron_config(28),
+  create_electron_config(29), create_electron_config(30),
+  create_electron_config(31), create_electron_config(32)
+};
--- main.c
+++ main.c
@@ -3,21 +3,13 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include "electron.h"
+#include "elements.h"
 
 int main() {
   printf("Periodic Table by LEGOAnimal22\n");
 
-  electron_config carbon = create_electron_config(6);
-  printf("%d, %d, %d, %d\n", carbon.atomic_number, carbon.highest_energy_level, carbon.valence_electrons, carbon.charge);
-
-  electron_config gold = create_electron_config(79);
-  printf("%d, %d, %d, %d\n", gold.atomic_number, gold.highest_energy_level, gold.valence_electrons, gold.charge);
-
-  electron_config radon = create_electron_config(86);
-  printf("%d, %d, %d, %d\n", radon.atomic_number, radon.highest_energy_level, radon.valence_electrons, radon.charge);
-
-  electron_config astatine = create_electron_config(85);
-  printf("%d, %d, %d, %d\n", astatine.atomic_number, astatine.highest_energy_level, astatine.valence_electrons, astatine.charge);
+  electron_config carbon = elements[5];
+  printf("%d,\t%d,\t%d,\t%d\n", carbon.atomic_number, carbon.highest_energy_level, carbon.valence_electrons, carbon.charge);
 
   return 1;
 }

b42eb09729de114cd3fb5cfb47a89ab8dde69103
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-20 15:14:45 -0600

    ...

--- main.c
+++ main.c
@@ -12,6 +12,6 @@ int main() {
   printf("%d,\t%d,\t%d,\t%d\n", carbon.atomic_number, carbon.highest_energy_level, carbon.valence_electrons, carbon.charge);
 
   element e_tungsten = create_element(TUNGSTEN);
-  printf("%s (%s): #%d; config: %d (highest energy level), %d valence electrons, charge of %d\n", e_tungsten.name, e_tungsten.symbol, e_tungsten.atomic_number, e_tungsten.configuration.highest_energy_level, e_tungsten.configuration.valence_electrons, e_tungsten.charge);
+  printf("%s (%s): #%d; config: %d (highest energy level), %d valence electrons, charge of %d\n", e_tungsten.name, e_tungsten.symbol, e_tungsten.atomic_number, e_tungsten.configuration.highest_energy_level, e_tungsten.configuration.valence_electrons, e_tungsten.configuration.charge);
   return 1;
 }

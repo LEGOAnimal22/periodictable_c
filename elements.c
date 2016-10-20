2664228bd0afd9cbaa652df4f2be84780e0e2b14
LEGOAnimal22 <buckley.daniel.n@gmail.com>
2016-10-20 15:09:32 -0600

    ??

--- elements.c
+++ elements.c
@@ -4,11 +4,9 @@
 #include "elements.h"
 
 element create_element(unsigned int atomic_number) {
-  char e_name[20] = element_names[atomic_number - 1];
-  char e_symbol[3] = element_symbols[atomic_number - 1];
   electron_config e_cnfg = create_electron_config(atomic_number);
 
-  element new_element = {e_name, e_symbol, atomic_number, e_cnfg};
+  element new_element = {element_names[atomic_number - 1], element_symbols[atomic_number - 1], atomic_number, e_cnfg};
 
   return new_element;
 }
